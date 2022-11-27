a747c26b32a936712dae0c51fc07e5d6612eb702
NelsonWang <wangnelson2@gmail.com>
2022-11-27 00:51:05 -0500

    auth

--- main.py
+++ main.py
@@ -1,18 +1,142 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI, Depends, File, UploadFile, Path, status, BackgroundTasks, HTTPException
+
+# from http.client import HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional, List
+from pydantic import BaseModel, Field
+
+import shortuuid
+from database import SessionLocal, engine
+import models
+import os, json
+from dotenv import load_dotenv
+from routes import utils
+
+from models import AuthDetails, AccountSchema
+from auth import AuthHandler
+
+import ast
+import boto3
+import awswrangler as wr
+import pandas as pd
+
+
+from pydantic import BaseModel
+from sqlalchemy import Column, String, Float, Integer
+
+from sqlalchemy.ext.declarative import declarative_base
+from fastapi_crudrouter import SQLAlchemyCRUDRouter
+
+load_dotenv()
+
+dotusername = os.getenv("USER")
 
 app = FastAPI()
 
+db = SessionLocal()
+
+Base = declarative_base()
+
+def get_db():
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    finally:
+        session.close()
+
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+profileRoutes = SQLAlchemyCRUDRouter(
+    schema=models.ProfileSchema,
+    create_schema=models.ProfileCreate,
+    update_schema=models.ProfileUpdate,
+    db_model = models.Profile,
+    db = get_db,
+    prefix= models.Profile.__tablename__
+)
+
+interactionRoutes = SQLAlchemyCRUDRouter(
+    schema=models.InteractionSchema,
+    create_schema=models.InteractionCreate,
+    db_model=models.Interaction,
+    db = get_db,
+    prefix=models.Interaction.__tablename__
+)
+
+
+app.include_router(profileRoutes)
+app.include_router(interactionRoutes)
+
+app.include_router(utils.router)
+
+
+
+models.Base.metadata.create_all(bind=engine)
+
 @app.get("/")
-async def root():
-    return {"message": "Ecommerce Welcome"}
+def index():
+    return { "message": "hello world This is the Another update version"}
 
-@app.get("/add/{num1}/{num2}")
-async def add(num1: int, num2: int):
-    """Add two numbers together"""
-
-    total = num1 + num2
-    return {"total": total}
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app, port=8080, host='0.0.0.0')
+
+
+auth_handler = AuthHandler()
+users = []
+
+@app.post('/register', status_code=201)
+def register(auth_details: AuthDetails):
+    if any(x['username'] == auth_details.username for x in users):
+        raise HTTPException(status_code=400, detail='Username is taken')
+    hashed_password = auth_handler.get_password_hash(auth_details.password)
+    users.append({
+        'username': auth_details.username,
+        'password': hashed_password    
+    })
+
+    mdAccount = models.Account( 
+        username= auth_details.username,
+        password= hashed_password 
+    )
+    
+    db.add(mdAccount)
+    db.commit()
+    return
+
+
+
+@app.post('/login')
+def login(auth_details: AuthDetails):
+    userAccount = None
+    
+    userAccount = db.query(models.Account).filter(models.Account.username == auth_details.username).first()
+    if(userAccount is None):
+        raise HTTPException(status_code=401, detail='Invalid username and/or password')
+    
+    if(userAccount is None) or (not auth_handler.verify_password(auth_details.password, userAccount.password)):
+        raise HTTPException(status_code=401, detail='Invalid username and/or password')
+
+    token = auth_handler.encode_token(userAccount.username)
+    return { 'token': token }
+
+
+@app.get('/unprotected')
+def unprotected():
+    return { 'hello': 'world' }
+
+
+@app.get('/protected')
+def protected(username=Depends(auth_handler.auth_wrapper)):
+    return { 'name': username }
---
+++ routes/utils.py
@@ -0,0 +1,72 @@
+from fastapi import  UploadFile, Path
+import models
+from fastapi import APIRouter
+from sqlalchemy.sql import exists, and_, or_
+import boto3
+from database import SessionLocal
+from enum import Enum;
+from pydantic import BaseModel
+import datetime
+from typing import Optional
+
+import os, json
+from dotenv import load_dotenv
+
+
+
+
+S3_BUCKET_NAME = 'myplatinumbucket'
+
+router = APIRouter(
+    prefix="/utils",
+    responses={404: {"description": "Not found"}},
+    tags=["Utils"]
+)
+
+
+
+@router.get("/welcome", status_code=200)
+async def getLogByID(id: str ):
+    
+    return {"Welcome": "Getting welcome message from: "+id}
+
+
+@router.post("/{id}/upload/")
+async def upload(id: str, file: UploadFile):
+    """
+    Uploads usign boto3 s3.
+    """
+
+    datet = datetime.datetime.now()
+    datetimepre = datet.strftime("%m%d%Y%H%M%S")
+    
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(S3_BUCKET_NAME)
+    filedir = f'manual_uploads/{id}/{datetimepre+file.filename}'
+    bucket.upload_fileobj(file.file, filedir)
+    uploaded_file_url = f"s3://{S3_BUCKET_NAME}/{filedir}"
+
+    
+    return {"message": f"Successfully uploaded {file.filename}", "location": uploaded_file_url}
+
+
+
+load_dotenv()
+
+dotusername = os.getenv("USER")
+@router.get('/getdot')
+async def getDotUsername():
+    return {"dotenv username": dotusername, "Unamked": os.getenv("UNMARKEDVARIABLE")}
+
+
+
+
+
+
+
+
+
+
+
+
+
